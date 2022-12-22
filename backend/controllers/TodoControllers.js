36f134a8cda5680777faca26018610255edf6d64
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2022-12-22 11:45:25 +0530

    task controllers

--- backend/controllers/TodoControllers.js
+++ backend/controllers/TodoControllers.js
@@ -51,3 +51,77 @@ exports.deleteTitle = async(req, res) => {
         })
     }
 }
+exports.createTask = async(req, res) => {
+    try {
+        const titleId = req.params.id;
+        if (!titleId) {
+            throw new Error('Unable to find title id')
+        }
+        const title = await Todo.findById(titleId);
+        const {task} = req.body;
+        title.tasks.push(task);
+        await title.save();
+        res.status(200).json({
+            success: true,
+            message: 'task added successfully',
+            title
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({
+            success: false,
+            message: 'Create task failed'
+        })
+    }
+}
+
+exports.getTasks = async (req, res) => {
+    try {
+        const tasks = await Todo.find()
+        res.status(200).json({
+            success:true,
+            titles
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({
+            success: false,
+            message: 'Unable to load Titles'
+        })
+    }
+}
+
+exports.editTask = async(req, res) => {
+    try {
+        
+        const task = await Todo.findByIdAndUpdate(req.params.id, req.body)
+        res.status(200).json({
+            success: true,
+            message: 'task updated successfully',
+            task
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({
+            success: false,
+            message: 'Unable to edit task'
+        })
+    }
+}
+
+exports.deleteTask = async (req, res) => {
+    try {
+        const task = await Todo.findByIdAndDelete(req.params.id)
+        res.status(200).json({
+            success: true,
+            message: 'Task deleted successfully'
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({
+            success: false,
+            message: 'Unable to delete task'
+        })
+    }
+}
